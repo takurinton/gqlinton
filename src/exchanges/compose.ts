3ba7332c61fcedc4a13a5e6d065beba4ffca6b4f
takurinton <taryochanmannn@icloud.com>
2021-06-15 00:51:54 +0900

    add: compose

--- src/exchanges/compose.ts
+++ src/exchanges/compose.ts
@@ -1,1 +1,25 @@
-export const composeExchanges = () => {};
+import { timeStamp } from 'console';
+import { Exchange, ExchangeInput } from '../types';
+
+// これは、何をする関数なのかわからないので注意深く観察する必要有りかもしれない。
+// 引数が Exchange のリストなので何かを作るまたは中継するみたいな感じなのかもしれない
+export const composeExchanges = (exchanges: Exchange[]) => ({
+  client,
+  forward,
+  dispatchDebug,
+}: ExchangeInput) =>
+exchanges.reduceRight(
+  (forward, exchange) =>
+    exchange({
+      client,
+      forward,
+      dispatchDebug(event) {
+        dispatchDebug({
+          timestamp: Date.now(),
+          source: exchange.name,
+          ...event,
+        });
+      },
+    }),
+  forward
+);
