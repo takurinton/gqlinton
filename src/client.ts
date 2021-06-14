77417101ece2c54bb6bef2603068bb53e39869db
takurinton <taryochanmannn@icloud.com>
2021-06-15 00:18:56 +0900

    add: base exchanges

---
+++ src/client.ts
@@ -0,0 +1,46 @@
+import {
+    filter,
+    make,
+    makeSubject,
+    onEnd,
+    onPush,
+    onStart,
+    pipe,
+    share,
+    Source,
+    take,
+    takeUntil,
+    publish,
+    subscribe,
+    switchMap,
+    fromValue,
+    merge,
+    map,
+    Subscription,
+} from 'wonka';
+import { TypedDocumentNode } from '@graphql-typed-document-node/core';
+import { DocumentNode } from 'graphql';
+
+import { composeExchanges, defaultExchanges } from './exchanges';
+import { fallbackExchange } from './exchanges/fallback';
+
+import {
+    Exchange,
+    ExchangeInput,
+    GraphQLRequest,
+    Operation,
+    OperationContext,
+    OperationResult,
+    OperationType,
+    RequestPolicy,
+    PromisifiedSource,
+    DebugEvent,
+} from './types';
+
+import {
+    createRequest,
+    stream,
+    typenameWrap,
+    noop,
+    makeOperation, 
+} from './utils';
---
+++ src/exchanges/compose.ts
@@ -0,0 +1,1 @@
+export const composeExchanges = () => {};
