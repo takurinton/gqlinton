ea97f05a4f2d866289da19ab73219537fa4559a6
takurinton <taryochanmannn@icloud.com>
2021-06-18 21:40:38 +0900

    fix: link

--- playground/example/index.ts
+++ playground/example/index.ts
@@ -1,21 +1,28 @@
 import { createClient, gql } from 'gqlinton/dist';
 
 const QUERY = gql`
-query postQuery($id: Int){
-  getPost (id: $id){
-    id
-    title
-    contents
-    pub_date
+query postQuery($pages: Int, $category: String){
+  getPosts (page: $pages, category: $category){
+    current
+    next
+    previous
+    category
+    results {
+      id
+      title
+      contents
+      category
+      pub_date
+    }
   }
 }
 `;
 
 const client = createClient({
-    url: 'http://localhost:8080/graphql',
+    url: 'https://api.takurinton.com/graphql',
 });
 
 client
-.query(QUERY, { id: 50 })
+.query(QUERY, { page: 1, category: '' })
 .toPromise()
 .then(res => console.log(res));
