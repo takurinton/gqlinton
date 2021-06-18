80c63a2f327f6d25bd1b66c9469dda3b8c664055
takurinton <taryochanmannn@icloud.com>
2021-06-18 21:35:20 +0900

    fix: Maximum call stack size exceeded

--- src/utils/typenames.ts
+++ src/utils/typenames.ts
@@ -18,13 +18,17 @@ const collectTypes = (
     obj: EntityLike | EntityLike[], 
     types: { [typename: string]: unknown }
 ) => {
-    if (Array.isArray(obj)) for (let i = 0; i < obj.length; i++) collectTypes(obj[i], types);
-    else {
+    if (Array.isArray(obj)) {
+        for (let i = 0; i < obj.length; i++) collectTypes(obj[i], types);
+    } else if (typeof obj === 'object' && obj !== null) {
         for (const key in obj) {
-            if (key === '__typename' && typeof obj[key] === 'string') types[obj[key] as string] = 0;
-            else collectTypes(obj[key], types);
-        };
-    };
+          if (key === '__typename' && typeof obj[key] === 'string') {
+            types[obj[key] as string] = 0;
+          } else {
+            collectTypes(obj[key], types);
+          }
+        }
+    }
 
     return types;
 };
